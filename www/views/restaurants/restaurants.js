0a29d36bf0a580c4fe52c9da26387179cf26a651
laning312 <286046620@qq.com>
2017-02-18 09:47:20 +0800

    无限滚动

--- www/views/restaurants/restaurants.js
+++ www/views/restaurants/restaurants.js
@@ -2,19 +2,29 @@
  * Created by asus on 2017/2/17.
  */
 angular.module("App")
-.controller("RestaurantsController", function($scope, $http){
-   $scope.page = 0;
-   $scope.total = 1;
-   $scope.restaurants = [];
+    .controller("RestaurantsController", function ($scope, $http) {
+        $scope.page = 0;
+        $scope.total = 1;
+        $scope.restaurants = [];
 
-   $scope.getRestaurants = function(){
-       $scope.page++;
-       $http.get("https://ionic-in-action-api.herokuapp.com/restaurants?age=" +$scope.page)
-           .success(function(data){
-                $scope.restaurants = data.restaurants;
-                console.log($scope.restaurants);
-           });
-   };
+        $scope.getRestaurants = function () {
+            $scope.page++;
+            console.log($scope.page);
+            $http.get("https://ionic-in-action-api.herokuapp.com/restaurants?page=" + $scope.page)
+                .success(function (data) {
+                    angular.forEach(data.restaurants, function (restaurant) {
+                        $scope.restaurants.push(restaurant);
+                    });
 
-   $scope.getRestaurants();
-});
+                    $scope.total = data.totalPages;
+
+                    $scope.$broadcast('scroll.infiniteScrollComplete');
+                })
+                .error(function (err) {
+                    $scope.$broadcast('scroll.infiniteScrollComplete');
+                    console.log(err);
+                });
+        };
+
+        $scope.getRestaurants();
+    });
